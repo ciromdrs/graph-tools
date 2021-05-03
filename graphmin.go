f20705531259cdb9ddc3b03740dd592bf71f21de
Ciro Medeiros <ciromoraismedeiros@gmail.com>
2021-05-03 09:11:56 -0300

    Update graphmin package comment

--- graphmin.go
+++ graphmin.go
@@ -1,4 +1,9 @@
 package graphmin
 
 /* Package graphmin implements solvers for the Formal-Language-Constrained Graph
-Minimization problem */
+Minimization (FLGM) problem */
+
+// GraphMinimizer solves the FLGM problem
+type GraphMinimizer interface {
+	minimize(Grammar, Graph, []Query, []Edge) Graph
+}
