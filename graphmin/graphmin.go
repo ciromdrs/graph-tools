d6ecb3b6c62df02b2963b6d1c6a7c2bb936a6862
Ciro Medeiros <ciromoraismedeiros@gmail.com>
2021-05-03 10:16:32 -0300

    Create basic interfaces in the graphmin module

--- graphmin/graphmin.go
+++ graphmin/graphmin.go
@@ -3,7 +3,32 @@ package graphmin
 /* Package graphmin implements solvers for the Formal-Language-Constrained Graph
 Minimization (FLGM) problem */
 
-// GraphMinimizer solves the FLGM problem
-type GraphMinimizer interface {
-	minimize(Grammar, Graph, []Query, []Edge) Graph
-}
+import (
+	"github.com/ciromdrs/graph-tools/ccfpq"
+	ds "github.com/ciromdrs/graph-tools/data_structures"
+)
+
+type (
+	// A GraphMinimizer solves the FLGM problem
+	GraphMinimizer interface {
+		minimize(ccfpq.Grammar, Graph, []Query, []Edge) Graph
+	}
+
+	// An Edge keeps track of AugItems where it appears
+	Edge interface {
+	}
+
+	// A Graph is a set of Edges
+	Graph interface {
+	}
+
+	// An AugItem is an augmented trace item for solving the FLGM proble
+	AugItem interface {
+	}
+
+	// A Query is a pair (vertex, symbol)
+	Query struct {
+		vertex ds.Vertex
+		symbol ds.Vertex
+	}
+)
